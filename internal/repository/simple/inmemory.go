0905d70f6139d03bf8997ec3286998fd038cc15e
e.birukov <jhnsft2010@gmail.com>
2021-09-07 19:22:27 +0300

    implement simple store [WIP]

--- internal/repository/simple/inmemory.go
+++ internal/repository/simple/inmemory.go
@@ -3,31 +3,62 @@ package simple
 import (
 	"errors"
 	"webrunner_configurator/internal/gen/model"
+	"webrunner_configurator/internal/repository"
 )
 
-type TaskConfigStore struct {
+var (
+	TaskConfigNotFound = errors.New("task config not found")
+)
+
+type taskConfigStore map[int64]model.TaskConfig
+
+func (s taskConfigStore) values() []model.TaskConfig {
+	values := make([]model.TaskConfig, len(s))
+	i := 0
+	for _, value := range s {
+		values[i] = value
+		i++
+	}
+	return values
 }
 
-func NewTaskConfigStore() *TaskConfigStore {
-	return &TaskConfigStore{}
+func NewTaskConfigStore() repository.TaskConfigRepository {
+	var store = make(taskConfigStore, 5)
+	return repository.TaskConfigRepository(store)
 }
 
-func (t TaskConfigStore) Create(config model.NewConfig) (int64, error) {
-	return 0, errors.New("implement me")
+func (s taskConfigStore) Create(config model.NewConfig) (int64, error) {
+	id := int64(len(s))
+	v := model.TaskConfig{
+		NewConfig: config,
+		Id:        id,
+	}
+	s[id] = v
+	return id, nil
 }
 
-func (t TaskConfigStore) Get(id int64) (*model.TaskConfig, error) {
-	return nil, errors.New("implement me")
+func (s taskConfigStore) Get(id int64) (*model.TaskConfig, error) {
+	if taskConfig, ok := s[id]; ok {
+		return &taskConfig, nil
+	} else {
+		return nil, TaskConfigNotFound
+	}
+
 }
 
-func (t TaskConfigStore) List() ([]model.TaskConfig, error) {
-	return nil, errors.New("implement me")
+func (s taskConfigStore) List() ([]model.TaskConfig, error) {
+	return s.values(), nil
 }
 
-func (t TaskConfigStore) Update(config model.TaskConfig) {
+func (s taskConfigStore) Update(config model.TaskConfig) {
 	panic("implement me")
 }
 
-func (t TaskConfigStore) Delete(id int64) error {
-	return errors.New("implement me")
+func (s taskConfigStore) Delete(id int64) error {
+	if _, ok := s[id]; ok {
+		delete(s, id)
+		return nil
+	} else {
+		return TaskConfigNotFound
+	}
 }
