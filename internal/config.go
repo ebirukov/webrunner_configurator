bbc2d80def32adcb9975646155d4fc7553235367
e.birukov <jhnsft2010@gmail.com>
2021-09-09 17:41:27 +0300

    add support env vars

--- internal/config.go
+++ internal/config.go
@@ -5,6 +5,9 @@ import (
 	"flag"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"os"
+	"strconv"
 )
 
 var (
@@ -34,13 +37,13 @@ type DBConfig struct {
 }
 
 func NewCommandlineConfig() *Config {
-	var port = flag.Int("port", 8080, "Port for HTTP server")
-	var dbName = flag.String("db.name", "mysql", "Database name")
-	var serverName = flag.String("db.host", "localhost:3306", "Database host")
-	var userName = flag.String("db.user", "root", "Database user")
-	var dbPassword = flag.String("db.password", "root", "Database user")
-	var apiKey = flag.String("apiKey", "qwerty123", "Authentication API key")
-	var authRole = flag.String("authRole", "analytic", "Authorization role define api permission")
+	var port = flag.Int("port", LookupEnvOrInt("WRC_PORT", 8080), "Port for HTTP server")
+	var dbName = flag.String("db.name", LookupEnvOrString("WRC_DB_NAME", "mysql"), "Database name")
+	var serverName = flag.String("db.host", LookupEnvOrString("WRC_DB_HOST", "localhost:3306"), "Database host")
+	var userName = flag.String("db.user", LookupEnvOrString("WRC_DB_USER", "root"), "Database user")
+	var dbPassword = flag.String("db.password", LookupEnvOrString("WRC_DB_PASSWORD", "root"), "Database user")
+	var apiKey = flag.String("apiKey", LookupEnvOrString("WRC_API_KEY", "qwerty123"), "Authentication API key")
+	var authRole = flag.String("authRole", LookupEnvOrString("WRC_AUTH_ROLE", "analytic"), "Authorization role define api permission")
 	flag.Parse()
 
 	dbConfig := DBConfig{*serverName, *userName, *dbPassword, *dbName}
@@ -51,6 +54,24 @@ func NewCommandlineConfig() *Config {
 		dbConfig,
 		SecurityConfig{AuthRole: *authRole, KeyAuthConfig: authValidator},
 	}
+}
+
+func LookupEnvOrString(key string, defaultVal string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultVal
+}
+
+func LookupEnvOrInt(key string, defaultVal int) int {
+	if val, ok := os.LookupEnv(key); ok {
+		v, err := strconv.Atoi(val)
+		if err != nil {
+			log.Fatalf("LookupEnvOrInt[%s]: %v", key, err)
+		}
+		return v
+	}
+	return defaultVal
 }
 
 func newKeyAuthValidator(apiKey string) func(s string, context echo.Context) (bool, error) {
